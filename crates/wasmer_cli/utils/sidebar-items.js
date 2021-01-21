51bfdca019c9851a141ac1df0ecdf7d367b3c97b
jubianchi <jubianchi@users.noreply.github.com>
2021-01-21 17:46:18 +0000

    deploy: e5ea746fd201fb7815134c7e5b2794077ce3d055

---
+++ crates/wasmer_cli/utils/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["parse_envvar","Parses a mapdir from an env var"],["parse_mapdir","Parses a mapdir from a string"],["retrieve_alias_pathbuf",""],["wasmer_should_print_color","Whether or not Wasmer should print with color"]]});
